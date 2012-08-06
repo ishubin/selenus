8b29f739f7452ac8ec0aeb2bee3b83688ee38b79
Ivan Shubin <ivan.ishubin@gmail.com>
2012-08-06 10:54:11 +0200

    Fixed makeDist script

--- makeDist.sh
+++ makeDist.sh
@@ -20,9 +20,7 @@ cp target/selenus-$versionInPom.jar dist/bin/selenus-$version.jar
 cp target/selenus-jar-with-dependencies.jar dist/bin/selenus-$version-all-dep.jar 
 cp LICENSE-2.0.txt dist/bin/.
 cp README dist/bin/.
-cp selenusMessages.properties dist/bin/. 
 cp experior.properties dist/bin/.
-cp ../oculus-experior/defaultMessages.properties dist/bin/.
 
 cd dist/bin
 zip -r -9 ../selenus-$version-bin.zip *
@@ -33,9 +31,7 @@ cp pom.xml dist/sources/.
 cp -r src dist/sources/src
 cp LICENSE-2.0.txt dist/sources/.
 cp README dist/sources/.
-cp selenusMessages.properties dist/sources/. 
 cp experior.properties dist/sources/.
-cp ../oculus-experior/defaultMessages.properties dist/sources/.
 
 cd dist/sources
 zip -r -9 ../selenus-$version-sources.zip *
